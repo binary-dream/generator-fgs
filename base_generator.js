d9e69816118a32854ed3ee2160811ee2a5403ed4
Marco Aurélio Prado <marco.pdsv@gmail.com>
2023-05-05 15:52:00 -0300

    refactor: log commands in a separated function

--- base_generator.js
+++ base_generator.js
@@ -8,29 +8,33 @@ class BaseGenerator extends Generator {
   welcomeLog({ showCommands }) {
     this.log(yosay(`Welcome to the ${chalk.blue("Flutter Glue Style")}!`));
     if (showCommands) {
-      this.log(`- Run 'yo fgs:component' to create a new component.`);
-      this.log(``);
-      this.log(
-        `    The program will ask for the screen name, if the component is shared between multiple screens, use 'shared'.`
-      );
-      this.log(`    The program will ask for the component name.`);
-      this.log(``);
-      this.log(`- Run 'yo fgs:di-container' to create the di container files.`);
-      this.log(``);
-      this.log(`- Run 'yo fgs:routes' to create the routes.dart file.`);
-      this.log(``);
-      this.log(`- Run 'yo fgs:screen' to create a new screen.`);
-      this.log(``);
-      this.log(`    The program will ask for the screen name.`);
-      this.log(``);
-      this.log(`- Run 'yo fgs:widget' to create a new widget.`);
-      this.log(``);
-      this.log(
-        `    The program will ask for the screen name, if the widget is shared between multiple screens, use 'shared'.`
-      );
-      this.log(`    The program will ask for the widget name.`);
-      this.log(``);
+      this._showCommands();
     }
+  }
+
+  _showCommands() {
+    this.log(`- Run 'yo fgs:component' to create a new component.`);
+    this.log(``);
+    this.log(
+      `    The program will ask for the screen name, if the component is shared between multiple screens, use 'shared'.`
+    );
+    this.log(`    The program will ask for the component name.`);
+    this.log(``);
+    this.log(`- Run 'yo fgs:di-container' to create the di container files.`);
+    this.log(``);
+    this.log(`- Run 'yo fgs:routes' to create the routes.dart file.`);
+    this.log(``);
+    this.log(`- Run 'yo fgs:screen' to create a new screen.`);
+    this.log(``);
+    this.log(`    The program will ask for the screen name.`);
+    this.log(``);
+    this.log(`- Run 'yo fgs:widget' to create a new widget.`);
+    this.log(``);
+    this.log(
+      `    The program will ask for the screen name, if the widget is shared between multiple screens, use 'shared'.`
+    );
+    this.log(`    The program will ask for the widget name.`);
+    this.log(``);
   }
 
   setPackageVars() {
