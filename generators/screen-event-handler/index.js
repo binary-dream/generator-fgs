76f36a617f70f2cb3c21d1657dcc557567799d0d
Marco Aurélio Prado <marco.pdsv@gmail.com>
2023-05-10 08:19:36 -0300

    feat: add screen event handler command

---
+++ generators/screen-event-handler/index.js
@@ -0,0 +1,181 @@
+"use strict";
+import BaseGenerator from "../../base_generator.js";
+import camel from "to-camel-case";
+import { upperCaseFirst } from "upper-case-first";
+
+class ScreenGenerator extends BaseGenerator {
+  async prompting() {
+    super.welcomeLog({ showCommands: false });
+
+    const prompts = [
+      {
+        type: "input",
+        name: "screenName",
+        message: "What is the name of the screen (use _ to separate words)?"
+      },
+      {
+        type: "input",
+        name: "eventName",
+        message: "What is the name of the event (use _ to separate words)?"
+      }
+    ];
+
+    await this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  default() {
+    super.setPackageVars();
+  }
+
+  writing() {
+    const screenNameLowerCase = this.props.screenName.toLowerCase();
+    const screenNameCamelCaseWithoutUnderline = camel(this.props.screenName);
+    const screenNameUpperCase = this.props.screenName.toUpperCase();
+    const eventNameLowerCase = this.props.eventName.toLowerCase();
+    const eventNameCamelCaseWithoutUnderline = camel(this.props.eventName);
+    const eventNameCamelCaseWithoutUnderlineFirstUpper = upperCaseFirst(
+      camel(this.props.eventName)
+    );
+
+    const templateData = {
+      screenNameUpperCase,
+      screenNameLowerCase,
+      packageName: this.packageName,
+      fgsUtilsPackageName: this.fgsUtilsPackageName,
+      screenNameCamelCaseWithoutUnderline,
+      eventNameLowerCase,
+      eventNameCamelCaseWithoutUnderlineFirstUpper,
+      eventNameCamelCaseWithoutUnderline
+    };
+
+    this._writeEventHandler({
+      templateData,
+      screenNameLowerCase,
+      eventNameLowerCase
+    });
+    this._writeEventHandlerOnBloc({
+      screenNameLowerCase,
+      screenNameUpperCase,
+      eventNameCamelCaseWithoutUnderline,
+      eventNameCamelCaseWithoutUnderlineFirstUpper
+    });
+    this._writeEvent({
+      screenNameLowerCase,
+      screenNameUpperCase,
+      eventNameCamelCaseWithoutUnderline,
+      eventNameCamelCaseWithoutUnderlineFirstUpper
+    });
+  }
+
+  _writeEventHandler({
+    templateData,
+    screenNameLowerCase,
+    eventNameLowerCase
+  }) {
+    this.fs.copyTpl(
+      this.templatePath("event_handler.dart.ejs"),
+      this.destinationPath(
+        `lib/screens/${screenNameLowerCase}/event_handlers/${screenNameLowerCase}__on_${eventNameLowerCase}_event_handler.dart`
+      ),
+      {
+        ...templateData,
+        importPackages: [
+          `import 'package:${this.packageName}/screens/${screenNameLowerCase}/${screenNameLowerCase}__screen_bloc.dart';`,
+          `import 'package:${this.packageName}/screens/${screenNameLowerCase}/${screenNameLowerCase}__screen_event.dart';`,
+          `import 'package:${this.packageName}/screens/${screenNameLowerCase}/${screenNameLowerCase}__screen_state.dart';`,
+          `import 'package:flutter_bloc/flutter_bloc.dart';`
+        ]
+          .sort()
+          .join("\n")
+      }
+    );
+  }
+
+  _writeEventHandlerOnBloc({
+    screenNameLowerCase,
+    screenNameUpperCase,
+    eventNameCamelCaseWithoutUnderline,
+    eventNameCamelCaseWithoutUnderlineFirstUpper
+  }) {
+    let blocPath = `lib/screens/${screenNameLowerCase}/${screenNameLowerCase}__screen_bloc.dart`;
+    let blocDataAsString;
+
+    try {
+      blocDataAsString = this.fs.read(this.destinationPath(blocPath), "utf8");
+    } catch (_) {
+      throw new Error(`${blocPath} file not found`);
+    }
+
+    const blocDataAsArray = blocDataAsString.split("\n");
+
+    const newBlocDataAsArray = [];
+
+    for (let i = 0; i < blocDataAsArray.length; i++) {
+      const line = blocDataAsArray[i];
+      if (
+        line ===
+        "        // --- EVENTS END ------------------------------------------------------"
+      ) {
+        newBlocDataAsArray.push(
+          `        ${eventNameCamelCaseWithoutUnderline}: (${eventNameCamelCaseWithoutUnderline}) => `,
+          `          ${screenNameUpperCase}__on${eventNameCamelCaseWithoutUnderlineFirstUpper}EventHandler(`,
+          `            bloc: this,`,
+          `            event: ${eventNameCamelCaseWithoutUnderline},`,
+          `            emit: emit,`,
+          `          ),`
+        );
+      }
+
+      newBlocDataAsArray.push(line);
+    }
+
+    this.fs.write(
+      this.destinationPath(blocPath),
+      newBlocDataAsArray.join("\n")
+    );
+  }
+
+  _writeEvent({
+    screenNameLowerCase,
+    screenNameUpperCase,
+    eventNameCamelCaseWithoutUnderline,
+    eventNameCamelCaseWithoutUnderlineFirstUpper
+  }) {
+    let eventPath = `lib/screens/${screenNameLowerCase}/${screenNameLowerCase}__screen_event.dart`;
+    let eventDataAsString;
+
+    try {
+      eventDataAsString = this.fs.read(this.destinationPath(eventPath), "utf8");
+    } catch (_) {
+      throw new Error(`${eventPath} file not found`);
+    }
+
+    const eventDataAsArray = eventDataAsString.split("\n");
+
+    const newEventDataAsArray = [];
+
+    for (let i = 0; i < eventDataAsArray.length; i++) {
+      const line = eventDataAsArray[i];
+      if (
+        line ===
+        "  // --- EVENTS END ------------------------------------------------------------"
+      ) {
+        newEventDataAsArray.push(
+          `  const factory ${screenNameUpperCase}__ScreenEvent.${eventNameCamelCaseWithoutUnderline}() = `,
+          `    ${screenNameUpperCase}__ScreenEvent__${eventNameCamelCaseWithoutUnderlineFirstUpper};`
+        );
+      }
+
+      newEventDataAsArray.push(line);
+    }
+
+    this.fs.write(
+      this.destinationPath(eventPath),
+      newEventDataAsArray.join("\n")
+    );
+  }
+}
+
+export default ScreenGenerator;
